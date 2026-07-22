a7ac4bb5c10c03824c457c70044e80f315c72f28
Brainfart Studio <mitchell@mitchellopitz.net>
2026-07-21 20:41:41 -0500

    feat - Add event_bus signal dispatch

--- autoload/event_bus.gd
+++ autoload/event_bus.gd
@@ -1,11 +1,37 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var _subscribers: Dictionary = {}
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func subscribe(event_type: String, handler: Callable) -> void:
+	if not _subscribers.has(event_type):
+		_subscribers[event_type] = []
+
+	var handlers: Array = _subscribers[event_type]
+	if not handlers.has(handler):
+		handlers.append(handler)
+
+
+func unsubscribe(event_type: String, handler: Callable) -> void:
+	if not _subscribers.has(event_type):
+		return
+
+	var handlers: Array = _subscribers[event_type]
+	handlers.erase(handler)
+
+
+func fire(event_type: String, event_data = null) -> void:
+	if not _subscribers.has(event_type):
+		return
+
+	var handlers: Array = _subscribers[event_type]
+	for i in range(handlers.size() - 1, -1, -1):
+		var handler: Callable = handlers[i]
+		if handler.is_valid():
+			handler.call(event_data)
+		else:
+			handlers.remove_at(i)
+
+
+func clear(event_type: String) -> void:
+	_subscribers.erase(event_type)

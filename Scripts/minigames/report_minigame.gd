d74cfd60a15d9d041da33dd0cd9f7610e4111a58
Yernoby <ybonreylouiejaugan@gmail.com>
2026-07-24 23:44:57 +0800

    report minigame changes
    changed the input to instead of space to fill in the report, you just click the report window and then it fills up the report. Now it sticks to actually being a point and click game

--- Scripts/minigames/report_minigame.gd
+++ Scripts/minigames/report_minigame.gd
@@ -2,27 +2,46 @@ extends Node2D
 class_name ReportMinigame
 
 @onready var report_label: Label = $ColorRect2/ReportLabel
-var blah_count: int = 0
-var max_blahs: int
+
+var report_words := {
+	"status": [
+		"Operational",
+		"Stable",
+		"Cooling",
+		"Pressure",
+		"Reactor",
+		"Temperature",
+		"Inspection",
+		"Maintenance",
+		"Routine",
+		"Complete",
+		"Report",
+		"Countdown"
+	]
+}
+
+var word_count: int = 0
+var max_words: int
 
 func _ready() -> void:
 	report_label.text = ""
-	max_blahs = randf_range(50,100)
+	max_words = randi_range(25, 50)
 	EventBus.fire("minigame_started", {"minigame_class": ReportMinigame})
 
 func _process(delta: float) -> void:
-	if blah_count >= max_blahs:
-		$ColorRect2/Submit.disabled = false
-	else:
-		$ColorRect2/Submit.disabled = true
-	if Input.is_action_just_pressed("ui_select") and blah_count < max_blahs:
-		var blahs_to_add = randi_range(2, 5)
-		for i in range(blahs_to_add):
-			if blah_count >= max_blahs:
-				break
-			report_label.text += "blah "
-			blah_count += 1
+	$ColorRect2/Submit.disabled = word_count < max_words
 
 func _on_submit_pressed() -> void:
 	EventBus.fire("minigame_completed", {"minigame_class": ReportMinigame})
 	queue_free()
+
+func _on_report_button_pressed() -> void:
+	var words = report_words["status"]
+	var words_to_add = randi_range(2, 5)
+
+	for i in range(words_to_add):
+		if word_count >= max_words:
+			break
+
+		report_label.text += words.pick_random() + " "
+		word_count += 1

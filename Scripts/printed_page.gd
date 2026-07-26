9a113df366d52007f5365dc1f768228c12c3fc55
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-07-26 12:59:43 +1200

    added the signed papers part

---
+++ Scripts/printed_page.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal page_signed
+
+@onready var page = $AnimatedSprite2D
+@onready var timer = $Timer
+
+func _on_printer_printed_page_clicked() -> void:
+	page.show()
+
+func _on_area_2d_input_event(event: InputEvent) -> void:
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	if event_is_mouse_click:
+		page.play("signed")
+		timer.start(0.5)
+		await timer.timeout
+		page.play("unsigned")
+		page.hide()
+		page_signed.emit()
--- Scripts/printer.gd
+++ Scripts/printer.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-# Either the player needs to click the print out
-# or once the printing finished we show the ui abruptly.
-# currently emits a signal when the user clicks.
-
 @onready var printer_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var printer_outline_sprite: Sprite2D = $Sprite2D
+
 @onready var printer_timer: Timer = $Timer
+@export var minimum_printer_time: float = 4.0
+@export var maximum_printer_time: float = 10.0
 
-@onready var mouseOver: bool = false
-@onready var printed: bool = false
-
+var mouseOver: bool = false
+var printed: bool = false
 
 signal PrintedPageClicked
+
+func _ready() -> void:
+	start_timer()
 
 func _on_area_2d_mouse_entered() -> void:
 	mouseOver = true
@@ -23,25 +24,27 @@ func _on_area_2d_mouse_exited() -> void:
 	printer_outline_sprite.hide()
 
 func _on_timer_timeout() -> void:
-	if !printed:
-		printer_sprite.play("printing")
-		await printer_sprite.animation_finished
-		printed = true
+	printer_sprite.play("printing")
+	await printer_sprite.animation_finished
+	printed = true
 
-# currently the other canvas layers don't pass on mouse events so.... this can't work yet
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	var event_is_mouse_click: bool = (
 		event is InputEventMouseButton and
 		event.button_index == MOUSE_BUTTON_LEFT and
 		event.is_pressed()
 	)
-	if event_is_mouse_click and mouseOver:
+	if event_is_mouse_click and mouseOver and printed:
 		PrintedPageClicked.emit()
 		print("Mouse clicked the printer")
 
-# ment to be called once the mini-game finished to reset state.
-func on_papers_signed():
+func start_timer() -> void:
+	# Pick a random number of seconds
+	var random_time = randf_range(minimum_printer_time, maximum_printer_time)
+	# Start the timer with random time number
+	printer_timer.start(random_time)
+
+func _on_printed_page_page_signed() -> void:
 	printed = false
 	printer_sprite.play("idle")
-	printer_timer.start()
-	
+	start_timer()

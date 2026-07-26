2d29ec4f0448d50d4473eeae6d9595badd50c4fe
EroAxe <eroaxe.business@gmail.com>
2026-07-25 20:47:36 -0600

    Quick Cleanup of Printer + PrinterPage
    Just double checking a few signals and such to make sure that it all works. It now loops printing pages on a timer (can be controlled w/ MinigameManager) that when clicked pop up a sheet to sign and react to click when signed.
    
    Implementation by DefinitelyMaybe tbc, just me tweaking it so it loops.

--- Scripts/printed_page.gd
+++ Scripts/printed_page.gd
@@ -8,7 +8,10 @@ signal page_signed
 func _on_printer_printed_page_clicked() -> void:
 	page.show()
 
-func _on_area_2d_input_event(event: InputEvent) -> void:
+func _on_area_2d_input_event(viewport, event: InputEvent, shape) -> void:
+	if !page.visible:
+		return
+	
 	var event_is_mouse_click: bool = (
 		event is InputEventMouseButton and
 		event.button_index == MOUSE_BUTTON_LEFT and
